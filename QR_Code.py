082a9c533534c2d6e9581e9affe3db28ca0c57c2
Krishna-KumarGupta <148635343+Krishna-KumarGupta@users.noreply.github.com>
2024-01-26 19:42:57 +0530

    QR_Code.py

--- QR_Code.py
+++ QR_Code.py
@@ -1,10 +1,23 @@
 import qrcode
 from PIL import Image
 
+# Define the data for the QR code
+data = "https://github.com/Krishna-KumarGupta?tab=repositories"
+
+# Create a QRCode object
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H,
-                   box_size=10,border=4,)
-qr.add_data("")
+                   box_size=10,
+                   border=4)
+
+# Add the data to the QRCode object
+qr.add_data(data)
+
+# Generate the QR code
 qr.make(fit=True)
-img=qr.make_image(fill_color="black",black_color="blue")
-img.save("Porgress.png")
+
+# Create an image from the QRCode object
+img = qr.make_image(fill_color="black", black_color="blue")
+
+# Save the image
+img.save("QRCode_GitHub.png")
